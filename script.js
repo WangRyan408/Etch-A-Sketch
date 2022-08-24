63f2a6017007549c2ec20b80c462a9f3fdab7518
Ryan Wang <rwryanwang@gmail.com>
2022-08-23 20:00:48 -0700

    Added default grid setup

--- script.js
+++ script.js
@@ -4,7 +4,41 @@ const numside = document.querySelector('#num');
 const btn = document.querySelector('#squareSide');
 const box = document.querySelector('#box');
 
+
 function generateGrid() {
+    const six = 16;
+
+    while(box.firstChild) {
+        box.removeChild(box.firstChild);
+    }
+
+    for (let i = 0; i < six; i++) {
+
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'grid-square');
+        newDiv.setAttribute('style', `outline: 1px solid; background-color: white; display: grid; grid-template-columns: repeat(${six}, 1fr);`);
+        newDiv.setAttribute('id', `row-${i}`);
+        box.appendChild(newDiv);
+
+     
+        
+    }
+
+    for (let i = 0; i < six; i++) {
+        const rowcell = document.querySelector(`#row-${i}`);
+        for (let j = 0; j < six; j++) {
+            const col = document.createElement('div');
+            col.setAttribute('class', 'grid-square');
+            col.setAttribute('style', 'outline: 1px solid; background-color: white; grid-column: auto;');
+            col.setAttribute('id', `col-${j}`);
+            rowcell.appendChild(col);
+        }
+    }
+
+}
+
+
+function customGrid() {
     let num = Number(numside['value']);
     while(box.firstChild) {
         box.removeChild(box.firstChild);
@@ -24,7 +58,6 @@ function generateGrid() {
 
     for (let i = 0; i < num; i++) {
         const rowcell = document.querySelector(`#row-${i}`);
-
         for (let j = 0; j < num; j++) {
             const col = document.createElement('div');
             col.setAttribute('class', 'grid-square');
@@ -43,7 +76,8 @@ function displayValue(value) {
     return console.log(value['value']);
 }
 
+generateGrid();
+//box.addEventListener('load', () => generateGrid());
+btn.addEventListener('click', () => customGrid());
 
-btn.addEventListener('click', () => generateGrid());
-
-console.log(numside.value);
+//console.log(numside.value);
