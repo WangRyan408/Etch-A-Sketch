b0b0ca37b45632714f04e28d69235db19f41c328
Ryan Wang <rwryanwang@gmail.com>
2022-08-23 19:15:30 -0700

    Added customizable grid functionality

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function generateGrid() {
 
         const newDiv = document.createElement('div');
         newDiv.setAttribute('class', 'grid-square');
-        newDiv.setAttribute('style', 'outline: 1px solid; background-color: white; display: flex; flex-direction: row;');
+        newDiv.setAttribute('style', `outline: 1px solid; background-color: white; display: grid; grid-template-columns: repeat(${num}, 1fr);`);
         newDiv.setAttribute('id', `row-${i}`);
         box.appendChild(newDiv);
 
@@ -22,13 +22,17 @@ function generateGrid() {
         
     }
 
-   /* for (let j = 0; j < num; j++) {
-        const rowcell = document.querySelector(`#row-${j}`);
-        const col = document.createElement('div');
-        col.setAttribute('class', 'grid-square');
-        col.setAttribute('style', 'outline: 1px solid; background-color: white; ');
-        rowcell.appendChild(col);
-    }*/
+    for (let i = 0; i < num; i++) {
+        const rowcell = document.querySelector(`#row-${i}`);
+
+        for (let j = 0; j < num; j++) {
+            const col = document.createElement('div');
+            col.setAttribute('class', 'grid-square');
+            col.setAttribute('style', 'outline: 1px solid; background-color: white; grid-column: auto;');
+            col.setAttribute('id', `col-${j}`);
+            rowcell.appendChild(col);
+        }
+    }
 }
 
 
